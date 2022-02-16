44a4978ab269d43177adead55ac1678cb5e302d4
Teddy <lamtlo820@gmail.com>
2022-02-16 15:55:54 -0800

    Add circular progess - not animated

--- src/components/about/Skills.jsx
+++ src/components/about/Skills.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CircularProgress from "./CircularProgress";
 
 const skillsContent = [
   { skillClass: "p80", skillPercent: "80", skillName: "PYTHON / DJANGO / FLASK" },
@@ -15,14 +16,21 @@ const Skills = () => {
   return (
     <>
       {skillsContent.map((val, i) => (
-        <div className="col-6 col-md-3 mb-3 mb-sm-5" key={i}>
-          <div className={`c100 ${val.skillClass}`}>
+        <div className="col-6 col-md-3 mb-3 mb-sm-5" key={i} align="center">
+          {/* <div className={`c100 ${val.skillClass}`}>
             <span>{val.skillPercent}%</span>
             <div className="slice">
               <div className="bar"></div>
               <div className="fill"></div>
             </div>
-          </div>
+          </div> */}
+
+          <CircularProgress
+            size={120}
+            strokeWidth={10}
+            percentage={val.skillPercent}
+          />
+
           <h6 className="text-uppercase open-sans-font text-center mt-2 mt-sm-4">
             {val.skillName}
           </h6>
